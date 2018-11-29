6545028441622069b8d649f3ff6d2044ca619a31
aditya11052 <adityamota5@gmail.com>
2018-11-29 11:43:01 -0500

    Banana
    added name

--- Day2.java
+++ Day2.java
@@ -1,6 +1,7 @@
 public class Day2{
 	public static void main (String[]args){
-		System.out.print("Ankit Chadha");
+		System.out.println("Ankit Chadha");
+		System.out.println("Aditya Mota");
 
 	}
 }
