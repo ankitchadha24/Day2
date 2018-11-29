de1c7b59242ee26f1684b968373aef43b92b6542
ankitchadha24 <36781509+ankitchadha24@users.noreply.github.com>
2018-11-29 12:35:17 -0500

    Update Day2.java

--- Day2.java
+++ Day2.java
@@ -1,48 +1,32 @@
 import java.util.ArrayList;
 public class Day2{
-	public static void main (String[]args){
 
-		int x = (int)(Math.random()*9999)+1;
+	public static ArrayList<Integer> convertToArrayList(int num){
+		ArrayList<Integer> list = new ArrayList<>();
+		while(num!=0){
+			list.add(0,num%10);
+			num/=10;
+			}
+		return list;
 
-		recreateInt(convertToArrayList(x));
+
 	}
-
-		
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static int recreateInt(ArrayList<Integer> X){
+	public static int recreateInt(ArrayList<Integer> p){
 			String O = "";
 
-			for(int i = 0; i < X.size; i++){
-				O += X.get(i);
+			for(int i = 0; i < p.size(); i++){
+				O += p.get(i);
 			}
 
 			return Integer.valueOf(O);
 
 	}
+	public static void main (String[]args){
+		int x = (int)(Math.random()*9999)+1;
+		System.out.println("The number is " +x);
+		System.out.println("The First Method:");
+		System.out.println(convertToArrayList(x));
+		System.out.println("The Second Method:");
+		System.out.println(recreateInt(convertToArrayList(x)));
+}
 }
